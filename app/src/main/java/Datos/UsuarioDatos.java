b2e413b5f23f1e82df7137459e24fa9d01c7dce3
0995356573123jeick@gmail.com <jescobar8142@uta.edu.ec>
2023-06-09 18:27:40 -0500

    Aida Cocha 18:27

--- app/src/main/java/Datos/UsuarioDatos.java
+++ app/src/main/java/Datos/UsuarioDatos.java
@@ -1,7 +1,55 @@
 package Datos;
 
-public class UsuarioDatos {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import androidx.annotation.Nullable;
+
+import DB_Helper.DbHelper_Usuarios;
+
+public class UsuarioDatos extends DbHelper_Usuarios {
+
+Context context;
 
 
-    
+    public UsuarioDatos(@Nullable Context context) {
+        super(context);
+
+        this.context=context;
+    }
+
+
+
+    public long insertarUsuario(String cedula,String nombre,String apellido, String correo,String clave){
+
+        long id=0;
+
+        try {
+            DbHelper_Usuarios dbHelper_usuarios=new DbHelper_Usuarios(context);
+            SQLiteDatabase db=dbHelper_usuarios.getWritableDatabase();
+
+            ContentValues values=new ContentValues();
+            values.put("cedula",cedula);
+            values.put("nombres",nombre);
+            values.put("apellido",apellido);
+            values.put("correo",correo);
+            values.put("clave",clave);
+
+            id=db.insert(TABLE_NOMBRE,null,values);
+
+
+        }catch (Exception e){
+            e.toString();
+        }
+        return  id;
+
+    }
+
+
+
+
+
+
+
 }
